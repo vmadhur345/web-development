5f5549955559683a72d34348718833c45ea3bbb5
vmadhur345 <98869674+vmadhur345@users.noreply.github.com>
2023-05-27 12:59:31 +0530

    Update script.js

--- dom/script.js
+++ dom/script.js
@@ -1,1 +1,25 @@
+const decrement =document.getElementById("decrement");
+const increment=document.getElementById("increment");
+const rest=document.getElementById("reset");
+const value=document.getElementById("value");
 
+decrement.addEventListener("click",()=>{
+    const ans=Number(value.innerText);
+    if(ans>0){
+        ans.innerText=value-1;
+    }else{
+        console.log("negative value not allowed");
+    }
+});
+increment.addEventListener("click",()=>{
+    const ans=Number(value.innerText);
+    if(value>=10){
+        console.log("values greater than 10 are not allowed");
+    }else{
+        value.innerText=ans+1;
+    }
+});
+
+rest.addEventListener("click",()=>{
+    value.innerText=0;
+})
